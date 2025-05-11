580b51e1c67298df5a624add99bedfa72d6be0ff
Bala-kumaran-S <balakumaran1325@gmail.com>
2025-05-11 08:43:59 +0530

    update

--- backend/src/main/java/com/NoBugs/backend/controller/HelloController.java
+++ backend/src/main/java/com/NoBugs/backend/controller/HelloController.java
@@ -1,13 +1,18 @@
-package com.edubug.backend.controller;
+package com.NoBugs.backend.controller;
 
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello from Spring Boot!";
+    }
+
+    @GetMapping("/")
+    public String work() {
+        return "Backend is working!";
     }
 }
